f3b49cdd0c810098e37094d7b56760535d614e3a
LiangYue <LiangYue1981816@Hotmail.com>
2017-06-04 22:16:24 +0800

    - Modify code

--- CrossEngine/src/RenderSystem/RenderVulkan/CrossVulkanInstance.cpp
+++ CrossEngine/src/RenderSystem/RenderVulkan/CrossVulkanInstance.cpp
@@ -99,72 +99,229 @@ namespace CrossEngine {
 
 	BOOL CVulkanInstance::Create(HINSTANCE hInstance, HWND hWnd, HDC hDC, uint32_t width, uint32_t height)
 	{
-		return TRUE;
+		try {
+			std::vector<const char*> enabledInstanceLayers;
+			std::vector<const char*> enabledInstanceExtensions;
+
+			CALL_VK_FUNCTION_THROW(EnumerateInstanceLayerProperties(enabledInstanceLayers));
+			CALL_VK_FUNCTION_THROW(EnumerateInstanceExtensionProperties(enabledInstanceExtensions));
+			CALL_VK_FUNCTION_THROW(CreateInstance(enabledInstanceLayers, enabledInstanceExtensions));
+			CALL_VK_FUNCTION_THROW(CreateDebugReportCallback());
+			CALL_VK_FUNCTION_THROW(CreatePresentationSurface(hInstance, hWnd));
+			CALL_VK_FUNCTION_THROW(CreateDevice());
+			CALL_VK_FUNCTION_THROW(CreateSwapchain(width, height, VK_SURFACE_TRANSFORM_INHERIT_BIT_KHR));
+
+			return TRUE;
+		}
+		catch (VkResult err) {
+			CVulkanInstance::SetLastError(err);
+			Destroy();
+
+			return FALSE;
+		}
 	}
 
 	void CVulkanInstance::Destroy(void)
 	{
-
+		DestroySwapchain();
+		DestroyDevice();
+		DestroyPresentationSurface();
+		DestroyDebugReportCallback();
+		DestroyInstance();
 	}
 
 	VkResult CVulkanInstance::EnumerateInstanceLayerProperties(std::vector<const char*> &enabledInstanceLayers) const
 	{
+		enabledInstanceLayers.clear();
+
+		uint32_t numLayers;
+		CALL_VK_FUNCTION_RETURN(vkEnumerateInstanceLayerProperties(&numLayers, NULL));
+
+		ASSERT(numLayers > 0);
+		std::vector<VkLayerProperties> layers(numLayers);
+		CALL_VK_FUNCTION_RETURN(vkEnumerateInstanceLayerProperties(&numLayers, layers.data()));
+
+		for (uint32_t index = 0; index < numLayers; index++) {
+#ifdef _DEBUG
+#define VK_LAYER_LUNARG_STANDARD_VALIDATION "VK_LAYER_LUNARG_standard_validation"
+			if (stricmp(layers[index].layerName, VK_LAYER_LUNARG_STANDARD_VALIDATION) == 0) {
+				enabledInstanceLayers.push_back(VK_LAYER_LUNARG_STANDARD_VALIDATION);
+				continue;
+			}
+#endif
+		}
+
 		return VK_SUCCESS;
 	}
 
 	VkResult CVulkanInstance::EnumerateInstanceExtensionProperties(std::vector<const char*> &enabledInstanceExtensions) const
 	{
+		enabledInstanceExtensions.clear();
+
+		uint32_t numExtensions;
+		CALL_VK_FUNCTION_RETURN(vkEnumerateInstanceExtensionProperties(NULL, &numExtensions, NULL));
+
+		ASSERT(numExtensions > 0);
+		std::vector<VkExtensionProperties> extensions(numExtensions);
+		CALL_VK_FUNCTION_RETURN(vkEnumerateInstanceExtensionProperties(NULL, &numExtensions, extensions.data()));
+
+		BOOL bSurfaceExtension = FALSE;
+		BOOL bPlatformSurfaceExtension = FALSE;
+		for (uint32_t index = 0; index < numExtensions; index++) {
+			if (stricmp(extensions[index].extensionName, VK_KHR_SURFACE_EXTENSION_NAME) == 0) {
+				enabledInstanceExtensions.push_back(VK_KHR_SURFACE_EXTENSION_NAME);
+				bSurfaceExtension = TRUE;
+				continue;
+			}
+
+#ifdef PLATFORM_WINDOWS
+			if (stricmp(extensions[index].extensionName, VK_KHR_WIN32_SURFACE_EXTENSION_NAME) == 0) {
+				enabledInstanceExtensions.push_back(VK_KHR_WIN32_SURFACE_EXTENSION_NAME);
+				bPlatformSurfaceExtension = TRUE;
+				continue;
+			}
+#endif
+
+#ifdef PLATFORM_ANDROID
+			if (stricmp(extensions[index].extensionName, VK_KHR_ANDROID_SURFACE_EXTENSION_NAME) == 0) {
+				enabledInstanceExtensions.push_back(VK_KHR_ANDROID_SURFACE_EXTENSION_NAME);
+				bPlatformSurfaceExtension = TRUE;
+				continue;
+			}
+#endif
+
+#ifdef _DEBUG
+			if (stricmp(extensions[index].extensionName, VK_EXT_DEBUG_REPORT_EXTENSION_NAME) == 0) {
+				enabledInstanceExtensions.push_back(VK_EXT_DEBUG_REPORT_EXTENSION_NAME);
+				continue;
+			}
+#endif
+		}
+
+		if (bSurfaceExtension == FALSE || bPlatformSurfaceExtension == FALSE) {
+			return VK_ERROR_INITIALIZATION_FAILED;
+		}
+
 		return VK_SUCCESS;
 	}
 
 	VkResult CVulkanInstance::CreateInstance(const std::vector<const char*> &enabledInstanceLayers, const std::vector<const char*> &enabledInstanceExtensions)
 	{
-		return VK_SUCCESS;
+		VkApplicationInfo appInfo = {};
+		appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+		appInfo.pNext = NULL;
+		appInfo.pApplicationName = TAG_NAME;
+		appInfo.applicationVersion = 1;
+		appInfo.pEngineName = TAG_NAME;
+		appInfo.engineVersion = 1;
+		appInfo.apiVersion = VK_API_VERSION_1_0;
+
+		VkInstanceCreateInfo createInfo = {};
+		createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+		createInfo.pNext = NULL;
+		createInfo.flags = 0;
+		createInfo.pApplicationInfo = &appInfo;
+		createInfo.enabledLayerCount = enabledInstanceLayers.size();
+		createInfo.ppEnabledLayerNames = enabledInstanceLayers.data();
+		createInfo.enabledExtensionCount = enabledInstanceExtensions.size();
+		createInfo.ppEnabledExtensionNames = enabledInstanceExtensions.data();
+		return vkCreateInstance(&createInfo, m_pAllocator->GetAllocationCallbacks(), &m_vkInstance);
 	}
 
 	VkResult CVulkanInstance::CreateDebugReportCallback(void)
 	{
+#ifdef _DEBUG
+		vkCreateDebugReportCallback = (PFN_vkCreateDebugReportCallbackEXT)vkGetInstanceProcAddr(m_vkInstance, "vkCreateDebugReportCallbackEXT");
+		vkDestroyDebugReportCallback = (PFN_vkDestroyDebugReportCallbackEXT)vkGetInstanceProcAddr(m_vkInstance, "vkDestroyDebugReportCallbackEXT");
+		if (vkCreateDebugReportCallback == NULL) return VK_ERROR_INITIALIZATION_FAILED;
+		if (vkDestroyDebugReportCallback == NULL) return VK_ERROR_INITIALIZATION_FAILED;
+
+		VkDebugReportCallbackCreateInfoEXT debugInfo = {};
+		debugInfo.sType = VK_STRUCTURE_TYPE_DEBUG_REPORT_CREATE_INFO_EXT;
+		debugInfo.pNext = NULL;
+		debugInfo.pUserData = NULL;
+		debugInfo.pfnCallback = DebugReportCallback;
+		debugInfo.flags =
+			VK_DEBUG_REPORT_INFORMATION_BIT_EXT |
+			VK_DEBUG_REPORT_DEBUG_BIT_EXT |
+			VK_DEBUG_REPORT_ERROR_BIT_EXT |
+			VK_DEBUG_REPORT_WARNING_BIT_EXT |
+			VK_DEBUG_REPORT_PERFORMANCE_WARNING_BIT_EXT;
+		return vkCreateDebugReportCallback(m_vkInstance, &debugInfo, m_pAllocator->GetAllocationCallbacks(), &m_vkDebugReportCallback);
+#else
 		return VK_SUCCESS;
+#endif
 	}
 
 	VkResult CVulkanInstance::CreatePresentationSurface(HINSTANCE hInstance, HWND hWnd)
 	{
-		return VK_SUCCESS;
+#ifdef PLATFORM_WINDOWS
+		VkWin32SurfaceCreateInfoKHR surfaceInfo = {};
+		surfaceInfo.sType = VK_STRUCTURE_TYPE_WIN32_SURFACE_CREATE_INFO_KHR;
+		surfaceInfo.pNext = NULL;
+		surfaceInfo.flags = 0;
+		surfaceInfo.hinstance = hInstance;
+		surfaceInfo.hwnd = hWnd;
+		return vkCreateWin32SurfaceKHR(m_vkInstance, &surfaceInfo, m_pAllocator->GetAllocationCallbacks(), &m_vkSurface);
+#endif
+
+#ifdef PLATFORM_ANDROID
+		VkAndroidSurfaceCreateInfoKHR surfaceInfo = {};
+		surfaceInfo.sType = VK_STRUCTURE_TYPE_ANDROID_SURFACE_CREATE_INFO_KHR;
+		surfaceInfo.pNext = NULL;
+		surfaceInfo.flags = 0;
+		surfaceInfo.window = (ANativeWindow *)hWnd;
+		return vkCreateAndroidSurfaceKHR(m_vkInstance, &surfaceInfo, m_pAllocator->GetAllocationCallbacks(), &m_vkSurface);
+#endif
 	}
 
 	VkResult CVulkanInstance::CreateDevice(void)
 	{
-		return VK_SUCCESS;
+		return m_pDevice->Create() ? VK_SUCCESS : VK_ERROR_INITIALIZATION_FAILED;
 	}
 
 	VkResult CVulkanInstance::CreateSwapchain(uint32_t width, uint32_t height, VkSurfaceTransformFlagBitsKHR transform)
 	{
-		return VK_SUCCESS;
+		return m_pSwapchain->Create(width, height, transform);
 	}
 
 	void CVulkanInstance::DestroyInstance(void)
 	{
+		if (m_vkInstance) {
+			vkDestroyInstance(m_vkInstance, m_pAllocator->GetAllocationCallbacks());
+		}
 
+		m_vkInstance = VK_NULL_HANDLE;
 	}
 
 	void CVulkanInstance::DestroyDebugReportCallback(void)
 	{
+#ifdef _DEBUG
+		if (vkDestroyDebugReportCallback && m_vkDebugReportCallback) {
+			vkDestroyDebugReportCallback(m_vkInstance, m_vkDebugReportCallback, m_pAllocator->GetAllocationCallbacks());
+		}
 
+		m_vkDebugReportCallback = VK_NULL_HANDLE;
+#endif
 	}
 
 	void CVulkanInstance::DestroyPresentationSurface(void)
 	{
+		if (m_vkSurface) {
+			vkDestroySurfaceKHR(m_vkInstance, m_vkSurface, m_pAllocator->GetAllocationCallbacks());
+		}
 
+		m_vkSurface = VK_NULL_HANDLE;
 	}
 
 	void CVulkanInstance::DestroyDevice(void)
 	{
-
+		m_pDevice->Destroy();
 	}
 
 	void CVulkanInstance::DestroySwapchain(void)
 	{
-
+		m_pSwapchain->Destroy();
 	}
 
 	VkInstance CVulkanInstance::GetInstance(void) const
