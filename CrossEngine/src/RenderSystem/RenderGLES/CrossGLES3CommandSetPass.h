ddb4fd4274e84ffcc2cc3e46fa7e05ccbc226767
LiangYue <LiangYue1981816@Hotmail.com>
2017-07-26 22:39:20 +0800

    - Modify code

--- CrossEngine/src/RenderSystem/RenderGLES/CrossGLES3CommandSetPass.h
+++ CrossEngine/src/RenderSystem/RenderGLES/CrossGLES3CommandSetPass.h
@@ -32,13 +32,15 @@ namespace CrossEngine {
 
 
 	protected:
-		CGLES3CommandSetPass(CGfxRenderPassPtr &ptrRenderPass, int indexPass)
+		CGLES3CommandSetPass(CGfxFrameBufferPtr &ptrFrameBuffer, CGfxRenderPassPtr &ptrRenderPass, int indexPass)
 			: m_indexPass(indexPass)
 		{
+			m_ptrFrameBuffer = ptrFrameBuffer;
 			m_ptrRenderPass = ptrRenderPass;
 		}
 		virtual ~CGLES3CommandSetPass(void)
 		{
+			m_ptrFrameBuffer.Release();
 			m_ptrRenderPass.Release();
 		}
 
@@ -46,11 +48,76 @@ namespace CrossEngine {
 	protected:
 		virtual void Execute(void) const
 		{
+			const CGLES3FrameBuffer *pFrameBuffer = (CGLES3FrameBuffer *)((CGfxFrameBuffer *)m_ptrFrameBuffer);
+			const CGLES3RenderPass *pRenderPass = (CGLES3RenderPass *)((CGfxRenderPass *)m_ptrRenderPass);
+			const GLSubpassInformation* pPass = pRenderPass->GetSubpass(m_indexPass);
 
+			for (const auto &itColorAttachment : pPass->colorAttachments) {
+				GLuint texture = pFrameBuffer->GetRenderTexture(itColorAttachment.first);
+				glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0 + itColorAttachment.first, GL_TEXTURE_2D, texture, 0);
+			}
+
+			{
+				GLuint texture = pFrameBuffer->GetRenderTexture(pPass->depthStencilAttachment);
+				GLenum format = pFrameBuffer->GetRenderTextureFormat(pPass->depthStencilAttachment);
+
+				if (CGLES3Helper::glIsFormatDepthOnly(format)) {
+					glFramebufferTexture2D(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_TEXTURE_2D, texture, 0);
+				}
+
+				if (CGLES3Helper::glIsFormatStencilOnly(format)) {
+					glFramebufferTexture2D(GL_FRAMEBUFFER, GL_STENCIL_ATTACHMENT, GL_TEXTURE_2D, texture, 0);
+				}
+
+				if (CGLES3Helper::glIsFormatDepthStencil(format)) {
+					glFramebufferTexture2D(GL_FRAMEBUFFER, GL_DEPTH_STENCIL_ATTACHMENT, GL_TEXTURE_2D, texture, 0);
+				}
+			}
+
+			glReadBuffer(GL_NONE);
+			CheckFramebufferStatus();
+		}
+
+		BOOL CheckFramebufferStatus(void) const
+		{
+			GLenum status = glCheckFramebufferStatus(GL_FRAMEBUFFER);
+
+			switch (status)
+			{
+			case GL_FRAMEBUFFER_COMPLETE:
+				break;
+
+			case GL_FRAMEBUFFER_INCOMPLETE_ATTACHMENT:
+				LOGE("[ERROR] Framebuffer incomplete: Attachment is NOT complete.\n");
+				break;
+
+			case GL_FRAMEBUFFER_INCOMPLETE_MISSING_ATTACHMENT:
+				LOGE("[ERROR] Framebuffer incomplete: No image is attached to FBO.\n");
+				break;
+
+			case GL_FRAMEBUFFER_INCOMPLETE_DIMENSIONS:
+				LOGE("[ERROR] Framebuffer incomplete: Attached images have different dimensions.\n");
+				break;
+
+			case GL_FRAMEBUFFER_INCOMPLETE_MULTISAMPLE:
+				LOGE("[ERROR] Framebuffer incomplete: Multisample.\n");
+				break;
+
+			case GL_FRAMEBUFFER_UNSUPPORTED:
+				LOGE("[ERROR] Framebuffer incomplete: Unsupported by FBO implementation.\n");
+				break;
+
+			default:
+				LOGE("[ERROR] Framebuffer incomplete: Unknown error.\n");
+				break;
+			}
+
+			return status == GL_FRAMEBUFFER_COMPLETE ? TRUE : FALSE;
 		}
 
 
 	protected:
+		CGfxFrameBufferPtr m_ptrFrameBuffer;
 		CGfxRenderPassPtr m_ptrRenderPass;
 		int m_indexPass;
 	};
