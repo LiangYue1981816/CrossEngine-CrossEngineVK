2f039dfa7d833b3134869bc0710f77d936188f43
LiangYue <LiangYue1981816@Hotmail.com>
2017-10-08 22:50:22 +0800

    - Modify code

--- CrossEngine/include/ResourceSystem/CrossMaterialPass.h
+++ CrossEngine/include/ResourceSystem/CrossMaterialPass.h
@@ -51,30 +51,8 @@ namespace CrossEngine {
 		const std::map<uint32_t, Uniform>& GetUniforms(void) const;
 
 	public:
-		BOOL Load(TiXmlNode *pPassNode);
+		BOOL Load(CStream *pStream);
 		BOOL PostLoad(void);
-
-	protected:
-		BOOL LoadShaderVertex(TiXmlNode *pPassNode);
-		BOOL LoadShaderFragment(TiXmlNode *pPassNode);
-		BOOL LoadPrimitiveTopology(TiXmlNode *pPassNode);
-		BOOL LoadTessellationPatchControlPoints(TiXmlNode *pPassNode);
-		BOOL LoadPolygonMode(TiXmlNode *pPassNode);
-		BOOL LoadCullMode(TiXmlNode *pPassNode);
-		BOOL LoadFrontFace(TiXmlNode *pPassNode);
-		BOOL LoadDepthClamp(TiXmlNode *pPassNode);
-		BOOL LoadDepthBias(TiXmlNode *pPassNode);
-		BOOL LoadRasterizerDiscard(TiXmlNode *pPassNode);
-		BOOL LoadSampleCounts(TiXmlNode *pPassNode);
-		BOOL LoadSampleShading(TiXmlNode *pPassNode);
-		BOOL LoadSampleAlphaToCoverage(TiXmlNode *pPassNode);
-		BOOL LoadSampleAlphaToOne(TiXmlNode *pPassNode);
-		BOOL LoadDepthTest(TiXmlNode *pPassNode);
-		BOOL LoadDepthBoundsTest(TiXmlNode *pPassNode);
-		BOOL LoadStencilTest(TiXmlNode *pPassNode);
-		BOOL LoadColorBlendLogic(TiXmlNode *pPassNode);
-		BOOL LoadColorBlendConstants(TiXmlNode *pPassNode);
-		BOOL LoadColorBlendAttachment(TiXmlNode *pPassNode);
 
 	public:
 		BOOL SetTexture(const char *szTextureName, uint32_t unit);
