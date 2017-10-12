be4fb6f3ca5eb63aea41f58c56f2906ddc0a15fa
LiangYue <LiangYue1981816@Hotmail.com>
2017-10-13 01:11:38 +0800

    - Modify code

--- UnitTesting/Base/Base.cpp
+++ UnitTesting/Base/Base.cpp
@@ -34,8 +34,8 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance
 	RECT rcView;
 	GetClientRect(hWnd, &rcView);
 
-	Engine()->Init(CrossEngine::GFX_API_GLES31, hInstance, hWnd, GetDC(hWnd), rcView, VK_SURFACE_TRANSFORM_IDENTITY_BIT_KHR, 4 * 1024 * 1024, 16, NULL, FALSE);
-//	Engine()->Init(CrossEngine::GFX_API_VULKAN, hInstance, hWnd, GetDC(hWnd), rcView, VK_SURFACE_TRANSFORM_IDENTITY_BIT_KHR, 4 * 1024 * 1024, 16, NULL, FALSE);
+//	Engine()->Init(CrossEngine::GFX_API_GLES31, hInstance, hWnd, GetDC(hWnd), rcView, VK_SURFACE_TRANSFORM_IDENTITY_BIT_KHR, 4 * 1024 * 1024, 16, NULL, FALSE);
+	Engine()->Init(CrossEngine::GFX_API_VULKAN, hInstance, hWnd, GetDC(hWnd), rcView, VK_SURFACE_TRANSFORM_IDENTITY_BIT_KHR, 4 * 1024 * 1024, 16, NULL, FALSE);
 	GfxDevice()->SetShaderCachePath("../Data/ShaderCache/");
 	GfxDevice()->AddShaderIncludePath("../Data/Shader/");
 	ResourceSystem()->PreLoadResourcePath("../Data");
