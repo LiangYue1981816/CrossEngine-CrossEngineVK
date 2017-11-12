f9319a668c906f7d0f2aa0aad09a7d31ee9ee838
LiangYue <LiangYue1981816@Hotmail.com>
2017-11-12 13:42:34 +0800

    - Modify code

--- UnitTesting/SubPass/SubPass.cpp
+++ UnitTesting/SubPass/SubPass.cpp
@@ -38,7 +38,7 @@ void CreateRenderer(void)
 	Renderer.ptrRenderPass->SetSubpassOutputColorReference(0, 1);
 	Renderer.ptrRenderPass->SetSubpassInputColorReference(1, 1);
 	Renderer.ptrRenderPass->SetSubpassOutputColorReference(1, 0);
-//	Renderer.ptrRenderPass->SetSubpassDependency(0, 0, 1);
+	Renderer.ptrRenderPass->SetSubpassDependency(0, 0, 1, VK_PIPELINE_STAGE_COLOR_ATTACHMENT_OUTPUT_BIT, VK_PIPELINE_STAGE_FRAGMENT_SHADER_BIT, VK_ACCESS_COLOR_ATTACHMENT_WRITE_BIT, VK_ACCESS_SHADER_READ_BIT, VK_DEPENDENCY_BY_REGION_BIT);
 	Renderer.ptrRenderPass->Create();
 
 	Renderer.ptrColorTexture = GfxDevice()->NewRenderTexture();
