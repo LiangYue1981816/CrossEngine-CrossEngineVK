b24bab51e060948430a7bd6e1283298e5e333c60
LiangYue <LiangYue1981816@Hotmail.com>
2018-04-22 18:16:36 +0800

    - 添加模型测试用例，资源载入

--- UnitTesting/Mesh/Mesh.cpp
+++ UnitTesting/Mesh/Mesh.cpp
@@ -6,12 +6,19 @@ CrossEngine::CDrawableStaticMesh *pMesh = NULL;
 
 void Create(void)
 {
+	ptrMaterial = MaterialManager()->LoadResource("PzVI-Tiger-P_Chassis.material", TRUE, TRUE);
+	ptrMesh = MeshManager()->LoadResource("PzVI-Tiger-P_Body.mesh", TRUE, TRUE);
 
+	pMesh = (CrossEngine::CDrawableStaticMesh *)RenderSystem()->AllocDrawable(CrossEngine::DRAWABLE_TYPE_STATIC_MESH);
+	pMesh->SetMesh(ptrMesh);
+	pMesh->SetMaterial(ptrMaterial);
 }
 
 void Destroy(void)
 {
-
+	RenderSystem()->FreeDrawable(pMesh);
+	ptrMaterial.Release();
+	ptrMesh.Release();
 }
 
 void Render(void)
