a45e25c08b19d7a2ea0afc59e6a8774296d59b03
LiangYue <LiangYue1981816@Hotmail.com>
2018-05-01 10:05:01 +0800

    - 调整代码

--- CrossEngine/src/RenderSystem/RenderGLES/CrossGLES3CommandBindDescriptorSet.h
+++ CrossEngine/src/RenderSystem/RenderGLES/CrossGLES3CommandBindDescriptorSet.h
@@ -32,11 +32,10 @@ namespace CrossEngine {
 
 
 	protected:
-		CGLES3CommandBindDescriptorSet(const CGfxDescriptorSetPtr &ptrDescriptorSet, const CGfxRenderPassPtr &ptrRenderPass, uint32_t indexPass)
-			: m_indexPass(indexPass)
+		CGLES3CommandBindDescriptorSet(const CGfxDescriptorSetPtr &ptrDescriptorSet, const CGfxPipelineGraphicsPtr &ptrPipelineGraphics)
 		{
 			m_ptrDescriptorSet = ptrDescriptorSet;
-			m_ptrRenderPass = ptrRenderPass;
+			m_ptrPipelineGraphics = ptrPipelineGraphics;
 		}
 
 
@@ -47,80 +46,15 @@ namespace CrossEngine {
 				return;
 			}
 
-			if (m_ptrRenderPass.IsNull() || m_ptrRenderPass->GetHandle() == NULL) {
+			if (m_ptrPipelineGraphics.IsNull() || m_ptrPipelineGraphics->GetHandle() == NULL) {
 				return;
-			}
-
-			GLuint indexTexUnit = 0;
-
-			const CGLES3DescriptorSet *pDescriptorSet = (CGLES3DescriptorSet *)((CGfxDescriptorSet *)m_ptrDescriptorSet);
-			const CGLES3RenderPass *pRenderPass = (CGLES3RenderPass *)((CGfxRenderPass *)m_ptrRenderPass);
-			const GLSubpassInformation* pSubPass = pRenderPass->GetSubpass(m_indexPass);
-			const CGLES3DescriptorSetLayout *pDescriptorSetLayout = pDescriptorSet->GetDescriptorSetLayout();
-
-			const std::map<uint32_t, std::map<uint32_t, uint32_t>> &uniformBlockBindings = pDescriptorSetLayout->GetUniformBlockBindings();
-			for (const auto &itUniformBlockBinding : uniformBlockBindings) {
-				for (const auto &itBinding : itUniformBlockBinding.second) {
-					GLuint program = itUniformBlockBinding.first;
-					GLuint binding = itBinding.first;
-					GLuint location = itBinding.second;
-
-					const CGfxUniformBufferPtr &ptrUniformBuffer = pDescriptorSet->GetUniformBuffer(binding);
-					if (ptrUniformBuffer.IsNull()) continue;
-
-					const uint32_t offset = pDescriptorSet->GetUniformBufferOffset(binding);
-					const VkDescriptorBufferInfo &bufferInfo = ptrUniformBuffer->GetDescriptorBufferInfo(pDescriptorSetLayout->GetSet(), binding);
-					glBindBufferRange(GL_UNIFORM_BUFFER, binding, (GLuint)ptrUniformBuffer->GetHandle(), bufferInfo.offset + offset, bufferInfo.range);
-					glUniformBlockBinding(program, location, binding);
-				}
-			}
-
-			const std::map<uint32_t, std::map<uint32_t, uint32_t>> &inputAttachmentBindings = pDescriptorSetLayout->GetInputAttachmentBindings();
-			for (const auto &itInputAttachmentBinding : inputAttachmentBindings) {
-				for (const auto &itBinding : itInputAttachmentBinding.second) {
-					GLuint program = itInputAttachmentBinding.first;
-					GLuint binding = itBinding.first;
-					GLuint location = itBinding.second;
-
-					const CGfxRenderTexturePtr &ptrRenderTexture = pDescriptorSet->GetRenderTexture(binding);
-					if (ptrRenderTexture.IsNull()) continue;
-
-					const CGLES3RenderTexture *pRenderTexture = (CGLES3RenderTexture *)((CGfxRenderTexture *)ptrRenderTexture);
-					glActiveTexture(GL_TEXTURE0 + indexTexUnit);
-					glBindSampler(indexTexUnit, pRenderTexture->GetSampler());
-					glBindTexture(pRenderTexture->GetTarget(), (GLuint)pRenderTexture->GetHandle());
-					glProgramUniform1i(program, location, indexTexUnit);
-
-					indexTexUnit++;
-				}
-			}
-
-			const std::map<uint32_t, std::map<uint32_t, uint32_t>> &sampledImageBindings = pDescriptorSetLayout->GetSampledImageBindings();
-			for (const auto &itSampledImageBinding : sampledImageBindings) {
-				for (const auto &itBinding : itSampledImageBinding.second) {
-					GLuint program = itSampledImageBinding.first;
-					GLuint binding = itBinding.first;
-					GLuint location = itBinding.second;
-
-					const CGfxTexturePtr &ptrTexture = pDescriptorSet->GetTexture(binding);
-					if (ptrTexture.IsNull()) continue;
-
-					const CGLES3Texture *pTexture = (CGLES3Texture *)((CGfxTexture *)ptrTexture);
-					glActiveTexture(GL_TEXTURE0 + indexTexUnit);
-					glBindSampler(indexTexUnit, pTexture->GetSampler());
-					glBindTexture(pTexture->GetTarget(), (GLuint)pTexture->GetHandle());
-					glProgramUniform1i(program, location, indexTexUnit);
-
-					indexTexUnit++;
-				}
 			}
 		}
 
 
 	protected:
 		CGfxDescriptorSetPtr m_ptrDescriptorSet;
-		CGfxRenderPassPtr m_ptrRenderPass;
-		uint32_t m_indexPass;
+		CGfxPipelineGraphicsPtr m_ptrPipelineGraphics;
 	};
 
 }
