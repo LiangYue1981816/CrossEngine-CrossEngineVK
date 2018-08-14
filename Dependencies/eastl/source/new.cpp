0d5f0ec5eb3b3dd491e68a437be56c9a82c6c050
LiangYue <LiangYue1981816@Hotmail.com>
2018-08-14 16:26:57 +0800

    + 添加EASTL

---
+++ Dependencies/eastl/source/new.cpp
@@ -0,0 +1,11 @@
+#include <stdlib.h>
+
+void* operator new[] (size_t size, const char* name, int flags, unsigned debugFlags, const char* file, int line)
+{
+	return malloc(size);
+}
+
+void* operator new[](size_t size, size_t alignment, size_t alignmentOffset, const char* name, int flags, unsigned debugFlags, const char* file, int line)
+{
+	return _aligned_offset_malloc(size, alignment, alignmentOffset);
+}
