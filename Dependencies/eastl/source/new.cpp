9573e195b1aa1c4cc870cc1948366096c918eae7
LiangYue <LiangYue1981816@Hotmail.com>
2018-08-16 17:10:13 +0800

    - 调整代码

--- Dependencies/eastl/source/new.cpp
+++ Dependencies/eastl/source/new.cpp
@@ -1,11 +1,15 @@
 #include <stdlib.h>
 
+void* alloc_mem(size_t size, const char* file, int line, bool is_array);
+
 void* operator new[] (size_t size, const char* name, int flags, unsigned debugFlags, const char* file, int line)
 {
-	return malloc(size);
+	return alloc_mem(size, file, line, true);
+//	return malloc(size);
 }
 
 void* operator new[](size_t size, size_t alignment, size_t alignmentOffset, const char* name, int flags, unsigned debugFlags, const char* file, int line)
 {
-	return _aligned_offset_malloc(size, alignment, alignmentOffset);
+	return alloc_mem(size, file, line, true);
+//	return _aligned_offset_malloc(size, alignment, alignmentOffset);
 }
